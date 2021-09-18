878b90c5059e0827a030b1e0f202d4e749590f13
TTrungNg <88432947+TTrungNg@users.noreply.github.com>
2021-09-18 14:19:40 +0700

    Create HiThree.java

---
+++ HiThree.java
@@ -0,0 +1,11 @@
+
+public class HiThree {
+    public static String printHello() {
+        String s = "HelloWorld";
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
+}
